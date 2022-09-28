74c25ba970afa34daf58dce75f818950064837ab
sebastianbello <sebtrejo@outlook.es>
2022-09-28 13:25:17 -0500

    Update FXMLMenu.fxml and FXMLMenuController.java files.

--- src/pollinatorconservation/views/FXMLMenuController.java
+++ src/pollinatorconservation/views/FXMLMenuController.java
@@ -14,7 +14,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
@@ -22,6 +24,9 @@ import javafx.stage.Stage;
  * @author sebtr
  */
 public class FXMLMenuController implements Initializable {
+
+    @FXML
+    private GridPane gridPaneSpecies;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -29,7 +34,11 @@ public class FXMLMenuController implements Initializable {
     }    
 
     @FXML
-    private void clickButtonLogin(MouseEvent event) throws IOException {
+    private void pressKeySearch(KeyEvent event) {
+    }
+
+    @FXML
+    private void clickLabelLogin(MouseEvent event) throws IOException {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         
         Scene scene = new Scene(FXMLLoader.load(getClass().getResource("FXMLLogin.fxml")));
