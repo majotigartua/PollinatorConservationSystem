7ee30b8904e7662d36dd2ab4408324c627b4d956
sebastianbello <sebtrejo@outlook.es>
2022-11-06 22:42:03 -0600

    Update PollinatorDAOTest.java file.

--- test/pollinatorconservation/model/dao/PollinatorDAOTest.java
+++ test/pollinatorconservation/model/dao/PollinatorDAOTest.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import pollinatorconservation.model.pojo.Pollinator;
-import pollinatorconservation.model.dao.PollinatorDAO;
 
 /**
  *
